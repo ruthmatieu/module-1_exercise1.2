0d3b566ac25ec6716ed55e931f87b2a6992124b3
Alanna Matieu <alannamatieu@M09-RMATIEU.lan>
2024-09-28 16:19:28 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num < 5) {
+            System.out.println("Welcome to Java");
+            num++;
+        }
+    }
+}
